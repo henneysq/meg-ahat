4934b2a3ea1b3fa5d7ddb32e76ce648e10d4f8ef
Mark <mah@optoceutics.com>
2023-11-28 09:58:10 +0100

    Added setup for installing as pkg

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="megahat",
+    version="0.1",
+    author="OptoCeutics",
+    author_email="mark@henney.dk",
+    description="Interaction with lamp firmware",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/henneysq/meg-ahat",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "pandas",
+    ],
+    python_requires='>=3.7',
+)
