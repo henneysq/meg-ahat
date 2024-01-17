d826788f7a6cb038d730d7cf46eae09301832585
Mark Henney <mark@henney.dk>
2024-01-17 15:08:19 +0100

    Added util for check bids validity

---
+++ util/validate_bids.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+from bids_validator import BIDSValidator
+
+PILOT_RAW1_PATH = Path("/project/3031004.01/pilot-data/raw1")
+
+bv = BIDSValidator()
+
+print(f"Validting files in {PILOT_RAW1_PATH}")
+invalid_files = []
+valid_files = []
+
+# for item in PILOT_RAW1_PATH.walk():
+#     if not bv.is_bids(item.as_posix()):
+#         invalid_files.append(item.as_posix())
+
+def check_if_dir_is_bids(dir):
+    for item in dir.iterdir():
+        if item.is_dir():
+            check_if_dir_is_bids(item)
+            continue
+        
+        relative_path_string_list = item.as_posix().split('/')[5:]
+        relative_path_string = "/".join(relative_path_string_list)
+        
+        if not bv.is_bids(relative_path_string):
+            invalid_files.append(item.as_posix())
+        else:
+            valid_files.append(item.as_posix())
+
+check_if_dir_is_bids(PILOT_RAW1_PATH)
+print("\nValid files:")
+[print(valid_file) for valid_file in valid_files]
+
+print("\nInvalid files:")
+[print(invalid_file) for invalid_file in invalid_files]
---
+++ util/validate_bids.sh
@@ -0,0 +1,3 @@
+module load anaconda3
+source activate /home/megmethods/markhen/.conda/envs/megahat-env/
+python3 /project/3031004.01/meg-ahat/util/validate_bids.py
