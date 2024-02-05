e148906a56996da92d5833390376a78b78a0aad1
Mark Henney <mark@henney.dk>
2024-02-05 10:30:01 +0100

    Print fieldtrip commit # at configuration

--- util/configure_ft.m
+++ util/configure_ft.m
@@ -1,2 +1,7 @@
-addpath '/home/common/matlab/fieldtrip'
+%%
+fieldtrip_dir = '~/matlab/fieldtrip';
+addpath(fieldtrip_dir);
 ft_defaults
+%%
+repo = gitrepo(fieldtrip_dir);
+fprintf('OBS.: Load Fieldtrip at Commit: %s\n', repo.CurrentBranch.LastCommit.ID)
