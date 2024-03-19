01d63a3627f1d5626adda1279ef340edf18e1907
Mark Henney <mark@henney.dk>
2024-03-19 12:07:13 +0100

    remove fieldtrip commit check

--- util/configure_ft.m
+++ util/configure_ft.m
@@ -3,5 +3,5 @@ fieldtrip_dir = '~/matlab/fieldtrip';
 addpath(fieldtrip_dir);
 ft_defaults
 %%
-repo = gitrepo(fieldtrip_dir);
-fprintf('OBS.: Load Fieldtrip at Commit: %s\n', repo.CurrentBranch.LastCommit.ID)
+%repo = gitrepo(fieldtrip_dir);
+%fprintf('OBS.: Load Fieldtrip at Commit: %s\n', repo.CurrentBranch.LastCommit.ID)
