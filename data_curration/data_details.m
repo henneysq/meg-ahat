b3c82ae3f10218792e17c17b39edb0b06b672841
Mark Henney <mark@henney.dk>
2024-04-05 15:28:04 +0200

    specify subject data peculiarities for curration

--- data_curration/data_details.m
+++ data_curration/data_details.m
@@ -1,8 +1,111 @@
 data_details_cfg = [];
 
+data_details_cfg.sub001 = [];
+data_details_cfg.sub001.run1 = [];
+data_details_cfg.sub001.run1.suffix = 'a042d115';
+data_details_cfg.sub001.run2 = [];
+data_details_cfg.sub001.run2.suffix = 'a042d115';
+
+
+data_details_cfg.sub002 = [];
+data_details_cfg.sub002.run1 = [];
+data_details_cfg.sub002.run1.suffix = 'a042d115';
+data_details_cfg.sub002.run2 = [];
+data_details_cfg.sub002.run2.suffix = 'a042d115';
+
+data_details_cfg.sub003 = [];
+data_details_cfg.sub003.run1 = [];
+data_details_cfg.sub003.run1.suffix = 'a042d115';
+data_details_cfg.sub003.run2 = [];
+data_details_cfg.sub003.run2.suffix = 'a042d115';
+
+data_details_cfg.sub004 = [];
+data_details_cfg.sub004.run1 = [];
+data_details_cfg.sub004.run1.suffix = 'a042d115';
+data_details_cfg.sub004.run2 = [];
+data_details_cfg.sub004.run2.suffix = 'a042d115';
+
+data_details_cfg.sub005 = [];
+data_details_cfg.sub005.run1 = [];
+data_details_cfg.sub005.run1.suffix = 'a042d115';
+data_details_cfg.sub005.run2 = [];
+data_details_cfg.sub005.run2.suffix = 'a042d115';
+
+data_details_cfg.sub006 = [];
+data_details_cfg.sub006.run1 = [];
+data_details_cfg.sub006.run1.suffix = 'b995195c';
+data_details_cfg.sub006.run2 = [];
+data_details_cfg.sub006.run2.suffix = 'b995195c';
+
+data_details_cfg.sub007 = [];
+data_details_cfg.sub007.run1 = [];
+data_details_cfg.sub007.run1.suffix = '234e5f40';
+data_details_cfg.sub007.run2 = [];
+data_details_cfg.sub007.run2.suffix = 'b2aef43d';
+
+data_details_cfg.sub008 = [];
+data_details_cfg.sub008.run1 = [];
+data_details_cfg.sub008.run1.suffix = '';
+data_details_cfg.sub008.run2 = [];
+data_details_cfg.sub008.run2.suffix = '';
+
+data_details_cfg.sub009 = [];
+data_details_cfg.sub009.run1 = [];
+data_details_cfg.sub009.run1.suffix = '';
+data_details_cfg.sub009.run2 = [];
+data_details_cfg.sub009.run2.suffix = '';
+
+data_details_cfg.sub010 = [];
+data_details_cfg.sub010.run1 = [];
+data_details_cfg.sub010.run1.suffix = 'cc66c65b';
+data_details_cfg.sub010.run2 = [];
+data_details_cfg.sub010.run2.suffix = 'cc66c65b';
+
+data_details_cfg.sub011 = [];
+data_details_cfg.sub011.run1 = [];
+data_details_cfg.sub011.run1.suffix = '';
+data_details_cfg.sub011.run2 = [];
+data_details_cfg.sub011.run2.suffix = '';
+
+data_details_cfg.sub012 = [];
+data_details_cfg.sub012.run1 = [];
+data_details_cfg.sub012.run1.suffix = 'd70eacfb';
+data_details_cfg.sub012.run2 = [];
+data_details_cfg.sub012.run2.suffix = 'd70eacfb';
+
+data_details_cfg.sub013 = [];
+data_details_cfg.sub013.run1 = [];
+data_details_cfg.sub013.run1.suffix = '';
+data_details_cfg.sub013.run2 = [];
+data_details_cfg.sub013.run2.suffix = '';
+
 data_details_cfg.sub014 = [];
 data_details_cfg.sub014.eyetrack_missing = true;
+data_details_cfg.sub014.run1 = [];
+data_details_cfg.sub014.run1.suffix = 'cc66c65b';
+data_details_cfg.sub014.run2 = [];
+data_details_cfg.sub014.run2.suffix = 'cc66c65b';
+
+data_details_cfg.sub015 = [];
+data_details_cfg.sub015.run1 = [];
+data_details_cfg.sub015.run1.suffix = 'fbd05d5a';
+data_details_cfg.sub015.run2 = [];
+data_details_cfg.sub015.run2.suffix = 'fbd05d5a';
+
+data_details_cfg.sub016 = [];
+data_details_cfg.sub016.run1 = [];
+data_details_cfg.sub016.run1.suffix = 'cc66c65b';
+data_details_cfg.sub016.run2 = [];
+data_details_cfg.sub016.run2.suffix = 'cc66c65b';
 
 data_details_cfg.sub019 = [];
-data_details_cfg.sub019.beh_run1_log_filename = ...
-    'sub-019_ses-001_run-001_experimentdata_managerdump-cc66c65b_00.csv';
+data_details_cfg.sub019.run1 = [];
+data_details_cfg.sub019.run1.suffix = 'cc66c65b_00';
+data_details_cfg.sub019.run2 = [];
+data_details_cfg.sub019.run2.suffix = 'cc66c65b';
+
+data_details_cfg.sub020 = [];
+data_details_cfg.sub020.run1 = [];
+data_details_cfg.sub020.run1.suffix = 'cc66c65b';
+data_details_cfg.sub020.run2 = [];
+data_details_cfg.sub020.run2.suffix = 'cc66c65b';
