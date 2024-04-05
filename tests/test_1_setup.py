d64de8dab00c1fab1cbb8ea3f2d3372b58046b74
Mark Henney <mark@henney.dk>
2024-04-05 08:44:39 +0200

    rename test_setup to ensure it runs first in discovery mode

--- tests/test_1_setup.py
+++ tests/test_1_setup.py
@@ -1,10 +1,16 @@
 import logging
 from pathlib import Path
 
+from tests.test_util import set_git_executable_path
+
+set_git_executable_path()
+
 import git
 
 test_log_dir = filename=Path(__file__).parent / "test_logs"
 test_log_dir.mkdir(exist_ok=True)
+test_data_dir = filename=Path(__file__).parent / "test_data"
+test_data_dir.mkdir(exist_ok=True)
 
 repo = git.Repo(search_parent_directories=True)
 sha = repo.head.object.hexsha[:8]
