3a3866634529ab825d8a7cd93e42df71e9c0a5ba
Mark Henney <mah@optoceutics.com>
2024-05-24 10:14:53 +0200

    fix reference to venv dir

--- util/format_and_push.sh
+++ util/format_and_push.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 # echo "${BASH_SOURCE[0]}"
 #echo "$( dirname -- "${BASH_SOURCE[-1]}" )"
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[-1]}" )" &> /dev/null && pwd )
-echo $SCRIPT_DIR
-for ITEM in $SCRIPT_DIR/*venv*/; do
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJ_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[-1]}" )" &> /dev/null && pwd )
+echo "$PROJ_DIR is the project directory"
+for ITEM in $PROJ_DIR/*venv*/; do
     if [[ -d $ITEM ]]; then
         echo "$ITEM is a directory"
         venv_dir=${ITEM%*/}
     fi
 done
 
+echo "$SCRIPT_DIR is where we find the update_readme.py"
 $venv_dir/bin/python3 $SCRIPT_DIR/update_readme.py
 
 git add README.md
