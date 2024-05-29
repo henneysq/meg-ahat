e57243ebd6f4f38c10a78a9a7f7761185b80187b
Mark Henney <mark@henney.dk>
2024-05-29 09:57:21 +0200

    fix try catch with malfunctioning syntax

--- util/reassemble_hemispheres.m
+++ util/reassemble_hemispheres.m
@@ -38,60 +38,62 @@ function reassembled_pos = reassemble_hemispheres(sources_left_h, sources_right_
 
             assert (all(reassembled_pos == reference.pos(:,d)))
         end
-    catch try
-        ids = [];
-        idrd = [];
-        idld = [];
-
-        stride_s = dim(1)*dim(3);
-        stride_d = dim(1)*dim(2)*2;
-        consec = dim(1);
-        for z = 1:dim(3)
-            for y = 1:dim(2)
-                ids = [ids (1:consec)+(y-1)*stride_s+(z-1)*consec];
-                idrd = [idrd (1:consec)+(y-1)*consec+(z-1)*stride_d];
-                idld = [idld (1:consec)+(y-1)*consec+(z-.5)*stride_d];
+    catch
+        try
+            ids = [];
+            idrd = [];
+            idld = [];
+    
+            stride_s = dim(1)*dim(3);
+            stride_d = dim(1)*dim(2)*2;
+            consec = dim(1);
+            for z = 1:dim(3)
+                for y = 1:dim(2)
+                    ids = [ids (1:consec)+(y-1)*stride_s+(z-1)*consec];
+                    idrd = [idrd (1:consec)+(y-1)*consec+(z-1)*stride_d];
+                    idld = [idld (1:consec)+(y-1)*consec+(z-.5)*stride_d];
+                end
             end
-        end
-
-        for d = 1:3
-            reassembled_pos = zeros(prod(dim)*2,1);
-            if d == 2
-                flipped_right = flip(sources_right_h.pos);
-                reassembled_pos(idrd) = flipped_right(ids,d+3);
-            else
-                reassembled_pos(idrd) = sources_right_h.pos(ids,d+3);
+    
+            for d = 1:3
+                reassembled_pos = zeros(prod(dim)*2,1);
+                if d == 2
+                    flipped_right = flip(sources_right_h.pos);
+                    reassembled_pos(idrd) = flipped_right(ids,d+3);
+                else
+                    reassembled_pos(idrd) = sources_right_h.pos(ids,d+3);
+                end
+                reassembled_pos(idld) = sources_left_h.pos(ids,d);
+                % figure;plot(reassembled_pos,'LineWidth',2);hold on; plot(reference.pos(:,d),'r--','LineWidth',2)
+    
+                assert (all(reassembled_pos == reference.pos(:,d)))
             end
-            reassembled_pos(idld) = sources_left_h.pos(ids,d);
-            figure;plot(reassembled_pos,'LineWidth',2);hold on; plot(reference.pos(:,d),'r--','LineWidth',2)
-
-            assert (all(reassembled_pos == reference.pos(:,d)))
-        end
-
-    catch % the above assertion - should be more specific
-        ids = [];
-        idrd = [];
-        idld = [];
-
-        stride = dim(1)*dim(2);
-        consec = stride;
-        for z = 1:dim(3)
-                ids = [ids (1:consec)+(z-1)*stride];
-                idrd = [idrd (1:consec)+(z-1)*stride*2];
-                idld = [idld (1:consec)+((z-1)+.5)*stride*2];
-        end
-
-
-        for d = 1:3
-            reassembled_pos = zeros(prod(dim)*2,1);
-            if d == 2
-                flipped_right = flip(sources_right_h.pos);
-                reassembled_pos(idrd) = flipped_right(ids,d+3);
-            else
-                reassembled_pos(idrd) = sources_right_h.pos(ids,d+3);
+    
+        catch % the above assertion - should be more specific
+            ids = [];
+            idrd = [];
+            idld = [];
+    
+            stride = dim(1)*dim(2);
+            consec = stride;
+            for z = 1:dim(3)
+                    ids = [ids (1:consec)+(z-1)*stride];
+                    idrd = [idrd (1:consec)+(z-1)*stride*2];
+                    idld = [idld (1:consec)+((z-1)+.5)*stride*2];
             end
-            reassembled_pos(idld) = sources_left_h.pos(ids,d);
-            assert (all(reassembled_pos == reference.pos(:,d)))
+    
+    
+            for d = 1:3
+                reassembled_pos = zeros(prod(dim)*2,1);
+                if d == 2
+                    flipped_right = flip(sources_right_h.pos);
+                    reassembled_pos(idrd) = flipped_right(ids,d+3);
+                else
+                    reassembled_pos(idrd) = sources_right_h.pos(ids,d+3);
+                end
+                reassembled_pos(idld) = sources_left_h.pos(ids,d);
+                assert (all(reassembled_pos == reference.pos(:,d)))
+            end
         end
     end
 
